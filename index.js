294d8549d45493a962d0d8d93efd7da264cc5dc7
Lucas Depret <lucas.depret@epitech.eu>
2020-03-20 20:08:27 +0100

    Msg 'c'est pas faux'

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ client.on('message', msg => {
   if (msg.content === 'arthour') {
     msg.reply('Cuillèèère !');
   }
+  if (msg.channel === "général"){
+      msg.reply({files:["https://media1.tenor.com/images/9e2fd8f6f208f6e34a01038859a10b0b/tenor.gif"]})
+  }
 });
 
 client.login('NjkwNTQ4NzAxODE0MzI1MjQ4.XnUMQQ.pqfGMW1bQp6XNY2MsQfBY6vh-Ac');

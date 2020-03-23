9cde668de93235fa4ab162f5b374b1529374b237
Lucas Depret <lucas.depret@epitech.eu>
2020-03-23 17:23:42 +0100

    fix

--- commands/albion.js
+++ commands/albion.js
@@ -2,15 +2,6 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 module.exports = class Albion{
-    command = {
-    'pvp':'Top PvP Fame, This Month',
-    'pve':'Top PvE Fame, This Month',
-    'gathering':'Top Gathering Fame, This Month',
-    'crafting': 'Top Crafting Fame, This Month'
-}
-
- rank = [':trophy: ', ':second_place: ', ':third_place: ',':yellow_square: ',":white_square:",
-            ":brown_square: ", ':yellow_circle: ', ':white_circle: ', ':brown_circle: ', ':poop: ']
 
     static parse(msg){
         const cmd = msg.content.split(' ');
@@ -20,12 +11,20 @@ module.exports = class Albion{
     }
 
  static infoRank(article, head = 5){ 
+     const command = {
+        'pvp':'Top PvP Fame, This Month',
+        'pve':'Top PvE Fame, This Month',
+        'gathering':'Top Gathering Fame, This Month',
+        'crafting': 'Top Crafting Fame, This Month'
+    }
+    const rank = [':trophy: ', ':second_place: ', ':third_place: ',':yellow_square: ',":white_square:",
+            ":brown_square: ", ':yellow_circle: ', ':white_circle: ', ':brown_circle: ', ':poop: '];
     request('https://www.albiononline2d.com/en/scoreboard/guilds/LvVjuhcgS1GDC60hSQo42A',(error,response,html)=>{
     if (!error && response.statusCode == 200){
-        var paddium = ':military_medal: '+ this.command[article]+ ' :military_medal:';
+        var paddium = ':military_medal: '+ command[article]+ ' :military_medal:';
         const $ = cheerio.load(html);
         titleTable= $(".card").each((i, card) =>{
-            if($(card).find('.card-header').text() == this.command[article]){
+            if($(card).find('.card-header').text() == command[article]){
                 $(card).find('tbody').children().each((y, players) =>{
                     var infos = []
                     if (y <= head){

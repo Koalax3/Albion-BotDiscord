58ad9438663c1bbc30c2c8f2ab421f308786749b
Lucas Depret <Koalax3@users.noreply.github.com>
2020-03-23 09:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+  }
   if (msg.content === 'arthour') {
     msg.reply('cuillièèère!');
   }

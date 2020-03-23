67723d51acbff56caf9e57bcba5d9d24c9780a73
Lucas Depret <Koalax3@users.noreply.github.com>
2020-03-23 17:00:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({
-    autorun:true
-});
+const client = new Discord.Client();
+const kaamelot = require('./commands/kaamelot');
+const albion = require('./commands/albion');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-  if (msg.content === 'arthour') {
-    msg.reply('cuillièèère!');
-  }
-  console.log(msg.member);
-  if (msg.channel.name === 'général' && !msg.member.user.bot && Math.floor(Math.random() * 11) == 4) {
-    msg.channel.send("c'est pas faux",{files:['https://i.giphy.com/XknChYwfPnp04.gif']});
-  }
+  kaamelot.common(msg);
+  albion.parse(msg);
 });
 
 client.login(process.env.BOT_TOKEN);
